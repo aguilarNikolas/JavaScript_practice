c46b7b2382be1687dae660de61fa991b36c6f57b
Nikolas Nascimento Aguilar <nik.adcd@gmail.com>
2023-08-03 14:53:57 -0300

    data types and declaring variables

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,14 @@
-alert('This works!');
+const  defaultResult = 0;
+let currentResult = defaultResult;
+
+currentResult = (currentResult + 10) * 3 / 2 - 1;
+
+// i can use string concatenation like pyhton
+// let calculationDescription = '('+  defaultResult + '+ 10) * 3 / 2 - 1';
+
+// I also can use  Dolar Signal and curly braces and back-ticks (usando crase), or breaking lines 
+let calculationDescription = `( ${defaultResult} + 10) * 3 / 2 - 1`;
+
+
+
+outputResult(currentResult,calculationDescription);
