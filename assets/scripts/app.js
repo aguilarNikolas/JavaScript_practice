cb9f02f605ccf5b9456a103365665a8ea6222bda
Nikolas Nascimento Aguilar <nik.adcd@gmail.com>
2023-08-08 10:18:33 -0300

    practcing funtions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,15 @@
 const  defaultResult = 0;
 let currentResult = defaultResult;
 
-currentResult = (currentResult + 10) * 3 / 2 - 1;
+function addNumbers(num1, num2){
+  const result = num1 + num2;
+//   alert(`The result is ${result}`);
+  return result
+}
+
+const additionResult = addNumbers(1,5);
+
+currentResult = additionResult;
 
 // i can use string concatenation like pyhton
 // let calculationDescription = '('+  defaultResult + '+ 10) * 3 / 2 - 1';
@@ -12,3 +20,4 @@ let calculationDescription = `( ${defaultResult} + 10) * 3 / 2 - 1`;
 
 
 outputResult(currentResult,calculationDescription);
+
---
+++ assignment_1/assignment.js
@@ -0,0 +1,11 @@
+let result;
+let userInput = 15;
+
+result = 18 + userInput;
+
+result = result / 3;
+result = result + 10;
+result = result - 1;
+
+alert(result);
+alert(userInput);
