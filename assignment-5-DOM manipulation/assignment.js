a13695bad5a7e7ba8f375472ca59cbd6d873250f
Nikolas Nascimento Aguilar <nik.adcd@gmail.com>
2023-08-28 16:09:45 -0300

    assignment 5 done

---
+++ assignment-5-DOM manipulation/assignment.js
@@ -0,0 +1,19 @@
+const task1Query = document.querySelector('#task-1');
+
+const task1SecondQuery = document.getElementById('task-1');
+
+task1Query.style.backgroundColor = 'black';
+task1Query.style.color = 'white';
+
+const title1 = document.querySelector('title');
+
+// const title2 =  document.head.textContent;
+const title2 = document.getElementsByTagName('title');
+
+// title2.textContent = 'Assignment - Solved!';
+title1.textContent = 'Assignment - Solved!';
+
+
+const h1 = document.querySelector('h1');
+
+h1.textContent = 'Assignment - Solved!';
